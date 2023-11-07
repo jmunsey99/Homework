b41e48cbac69442eaa4fa686b46f547f5921fecd
jmunsey99 <jordan_munsey26@milton.edu>
2023-11-07 13:12:08 -0500

    My loops homework

---
+++ src/Loops.java
@@ -0,0 +1,39 @@
+public class Loops {
+    public static void main(String[] args) {
+Loops myCount = new Loops();
+    }
+
+    public Loops (){
+    System.out.println("First Challenge");
+        countUp();
+    System.out.println("Second challenge");
+    countByThrees();
+    System.out.println("Third challenge");
+    countDown();
+    System.out.println("Happy Loop Year!");
+
+    }// end of loops method
+
+    public void countUp (){
+
+        for (int i = 1; i <= 5; i = i+1){
+            System.out.println(i);
+        }
+
+
+    }
+    public void countByThrees () {
+        for (int i = 3; i <= 15; i = i + 3) {
+            System.out.println(i);
+        }
+
+    }
+public void countDown (){
+        for (int i = 10; i >= 1; i = i-1){
+            System.out.println(i);
+        }
+
+}
+
+
+}// end of loops class
